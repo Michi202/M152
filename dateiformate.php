c8b750da1fa73b3f262923def61310f80f915324
Michi202 <michael.marchesi@edu.tbz.ch>
2022-07-08 14:08:12 +0200

    dateiformate.php

---
+++ dateiformate.php
@@ -0,0 +1,153 @@
+<?php include("header.php") ?>
+<div id="main">
+    <div class="inner">
+        <header>
+            <h1>Dateiformate</h1>
+        </header>
+        <section>
+            <h2>Originalbilder</h2>
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/wall.jpg" width="300px" height="300px">
+                    <p class="no-margin">Wand</p>
+                    <p class="no-margin">65 KB</p>
+                    <p class="no-margin">JPG</p>
+                </div>
+                <div class="col-6">
+                    <img src="images/blumenbeet.jpg" width="300px" height="300px">
+                    <p class="no-margin">Blumenbeet</p>
+                    <p class="no-margin">172 KB</p>
+                    <p class="no-margin">JPG</p>
+                </div>
+            </div>
+            <br/>
+            <h3>BMP</h3>
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/BMP/wall.bmp" width="300px" height="300px">
+                    <p class="no-margin">Wand</p>
+                    <p class="no-margin">801 KB</p>
+                    <p class="no-margin">BMP</p>
+                </div>
+                <div class="col-6">
+                    <img src="images/BMP/blumenbeet.bmp" width="300px" height="300px">
+                    <p class="no-margin">Blumenbeet</p>
+                    <p class="no-margin">901 KB</p>
+                    <p class="no-margin">BMP</p>
+                </div>
+            </div>
+            <br/>
+            <h3>GIF</h3>
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/GIF/wall.gif" width="300px" height="300px">
+                    <p class="no-margin">Wand</p>
+                    <p class="no-margin">233 KB</p>
+                    <p class="no-margin">GIF</p>
+                </div>
+                <div class="col-6">
+                    <img src="images/GIF/blumenbeet.gif" width="300px" height="300px">
+                    <p class="no-margin">Blumenbeet</p>
+                    <p class="no-margin">291 KB</p>
+                    <p class="no-margin">GIF</p>
+                </div>
+            </div>
+            <br/>
+            <h3>JPG Low Quality</h3>
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/JPG low qual/wall.jpg" width="300px" height="300px">
+                    <p class="no-margin">Wand</p>
+                    <p class="no-margin">57 KB</p>
+                    <p class="no-margin">JPG</p>
+                </div>
+                <div class="col-6">
+                    <img src="images/JPG low qual/blumenbeet.jpg" width="300px" height="300px">
+                    <p class="no-margin">Blumenbeet</p>
+                    <p class="no-margin">135 KB</p>
+                    <p class="no-margin">JPG</p>
+                </div>
+            </div>
+            <br/>
+            <h3>PNG-8</h3>
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/PNG-8/wall.png" width="300px" height="300px">
+                    <p class="no-margin">Wand</p>
+                    <p class="no-margin">114 KB</p>
+                    <p class="no-margin">PNG</p>
+                </div>
+                <div class="col-6">
+                    <img src="images/PNG-8/blumenbeet.png" width="300px" height="300px">
+                    <p class="no-margin">Blumenbeet</p>
+                    <p class="no-margin">179 KB</p>
+                    <p class="no-margin">PNG</p>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <h2>Originalvideo</h2>
+            <div class="row">
+                <div class="col-12">
+                <video width="320" height="260" controls>
+                <source src="images/stars.mp4" type="video/mp4">
+                </video> 
+                    <p class="no-margin">Sterne</p>
+                    <p class="no-margin">84.712 MB</p>
+                    <p class="no-margin">MP4</p>
+                </div>
+            </div>
+            </br>
+            <h2>WEBM</h2>
+            <div class="row">
+                <div class="col-12">
+                <video width="320" height="260" controls>
+                <source src="images/WEBM/stars.webm" type="video/webm">
+                </video> 
+                    <p class="no-margin">Sterne</p>
+                    <p class="no-margin">150.674 MB</p>
+                    <p class="no-margin">WEBM</p>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <h2>Originalaudio</h2>
+            <div class="row">
+                <div class="col-12">
+                <audio controls>
+                <source src="images/home.mp3" type="audio/mp3">
+                </audio> 
+                    <p class="no-margin">Home</p>
+                    <p class="no-margin">278 KB</p>
+                    <p class="no-margin">MP3</p>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="row">
+                <div class="col-12">
+                <audio controls>
+                <source src="images/M4A/home.m4a" type="audio/m4a">
+                </audio> 
+                    <p class="no-margin">Home</p>
+                    <p class="no-margin">144 KB</p>
+                    <p class="no-margin">M4A</p>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="row">
+                <div class="col-12">
+                <audio controls>
+                <source src="images/WAV/home.wav" type="audio/wav">
+                </audio> 
+                    <p class="no-margin">Home</p>
+                    <p class="no-margin">1490 KB</p>
+                    <p class="no-margin">WAV</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+<?php include("footer.php") ?>
